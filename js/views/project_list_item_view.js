1f4d7fd27fdcebc6f6c666a71f2ca011715f5da6
Chris Jobling <c.p.jobling@swansea.ac.uk>
2013-04-23 00:04:03 +0100

    Add skeleton code for select action

--- js/views/project_list_item_view.js
+++ js/views/project_list_item_view.js
@@ -13,7 +13,7 @@ app.ProjectListItemView = Backbone.View.extend({
 
     // The DOM events specific to an item
     events: {
-
+        'click [type="checkbox"]': 'selectProject'
     },
     
     // Initialize the view by assigning events to content and initializing
@@ -27,5 +27,23 @@ app.ProjectListItemView = Backbone.View.extend({
         this.$el.html( this.template( this.model.toJSON() ) );
         return this;
     },
+
+    selectProject: function() {
+        // extract checkbox element
+        $checkbox = this.$('[type="checkbox"]');
+        // get the associated project id
+        p_id = $checkbox.val();
+        if ($checkbox.prop('checked')) {
+            alert('Project ' + p_id + '  selected');
+            // TODO: change class of associated button
+            // add project with id=p_id to selected projects list
+        }
+        else {
+            alert('Project ' + p_id + '  de-selected');
+            //  was previously selected, restore style
+            //  remove selected project from selected project list
+        }
+        // make order button depend on number of selected projects
+    }
 
 });
